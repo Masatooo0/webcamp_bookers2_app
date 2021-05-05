9b6d1c2e984a8c373443fd711d354a4a7999c2c4
“Masatooo0” <masato.n.0321@gmail.com>
2021-05-05 13:29:51 +0000

    [add]部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,14 @@
 class BooksController < ApplicationController
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
     redirect_to books_path
   end
 
